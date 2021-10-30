1cd0773d071621b356b5e1e72080f33885d59e7d
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 21:45:45 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["Corpus/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-Corpus-Cy',
-    version='1.0.4',
+    version='1.0.5',
     packages=['Corpus'],
     package_data={'Corpus': ['*.pxd', '*.pyx', '*.c', '*.py']},
     url='https://github.com/StarlangSoftware/Corpus-Cy',
@@ -13,5 +17,7 @@ setup(
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Corpus library',
-    install_requires=['NlpToolkit-Dictionary-Cy']
+    install_requires=['NlpToolkit-Dictionary-Cy'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
