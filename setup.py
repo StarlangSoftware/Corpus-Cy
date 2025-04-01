dba53c44334ed6b5d824135f213e747e932cbec3
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2025-04-01 21:27:07 +0300

    Updated name and version.

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize(["Corpus/*.pyx"],
                           compiler_directives={'language_level': "3"}),
-    name='NlpToolkit-Corpus-Cy',
-    version='1.0.22',
+    name='nlptoolkit-corpus-cy',
+    version='1.0.23',
     packages=['Corpus'],
     package_data={'Corpus': ['*.pxd', '*.pyx', '*.c', '*.py']},
     url='https://github.com/StarlangSoftware/Corpus-Cy',
